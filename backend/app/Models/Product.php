ac65a57605904e46a26287dc425070d270b3753c
vunguyen-cadabra <nguyen.tien.rcvn2012@gmail.com>
2021-12-13 17:07:19 +0700

    add

---
+++ backend/app/Models/Product.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Product extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'productName', 'categoryId', 'price','description','inventory','updated_at','imageUrl'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+    ];
+
+    /**
+     * @param $query
+     * @param $name
+     * @return mixed
+     */
+    public function scopeCategoryName($query, $categoryId)
+    {
+        return $query->join('category', 'category.id', '=', 'products.categoryId')->select('categoryName')->get();
+
+    }
+
+    /**
+     * @param $query
+     * @param $email
+     * @return mixed
+     */
+    public function scopeEmail($query, $email)
+    {
+        return $query->where('users.email', 'LIKE', "%$email%", 'or');
+    }
+}
---
+++ backend/database/migrations/2021_12_10_062442_create_order_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrderTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order', function (Blueprint $table) {
+            $table->id();
+            $table->string('userId');
+            $table->integer('productId');
+            $table->integer('quantily');
+            $table->integer('totalPrice');
+            $table->integer('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order');
+    }
+}
